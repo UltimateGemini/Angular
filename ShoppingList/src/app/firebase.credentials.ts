67f7ac341edafa4651627658d9aa3a9583489a75
Rotor7 <Rotor7@users.noreply.github.com>
2017-10-18 11:38:33 -0400

    Project setup
    - Installed dependencies: Firebase, Angularfire2
    - Set up project in Firebase
    - Created ShoppingListPage
    - Created AddShoppingListPage

---
+++ ShoppingList/src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CREDENTIALS = {
+    apiKey: "AIzaSyCsmyzUzpkeFSYdKWg3JAQQ_bG2ived91Q",
+    authDomain: "shopping-list-30b68.firebaseapp.com",
+    databaseURL: "https://shopping-list-30b68.firebaseio.com",
+    projectId: "shopping-list-30b68",
+    storageBucket: "shopping-list-30b68.appspot.com",
+    messagingSenderId: "286169567268"
+};
---
+++ ShoppingList/src/pages/shopping-list/shopping-list.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { AddShoppingPage } from '../add-shopping/add-shopping';
+
+@Component({
+  selector: 'page-shopping-list',
+  templateUrl: 'shopping-list.html',
+})
+export class ShoppingListPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  navigateToAddShoppingPage() {
+    this.navCtrl.push(AddShoppingPage);
+  }
+
+}
