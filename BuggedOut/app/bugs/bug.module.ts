1cbccc1b6d0e53c55e52e49e0ba6ce611239dee5
Noel-F <2591142+Noel-F@users.noreply.github.com>
2018-02-19 16:59:30 -0500

    Created Custom Component
    - Bug List Component

--- BuggedOut/app/bugs/bug.module.ts
+++ BuggedOut/app/bugs/bug.module.ts
@@ -1,11 +1,15 @@
 import {NgModule} from "@angular/core";
 import {SharedModule} from "../shared/shared.module";
+import {BugListComponent} from "./bug-list/bug-list.component";
 
 @NgModule({
     imports: [SharedModule],
-    declarations: [],
+    declarations: [
+        BugListComponent
+    ],
     exports: [],
     providers: []
 })
 
-export class BugModule {}
+export class BugModule {
+}
