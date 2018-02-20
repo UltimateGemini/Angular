1e29eb8cc82092dbe652c26af121cb203ca4db1d
Noel-F <2591142+Noel-F@users.noreply.github.com>
2018-02-20 09:27:02 -0500

    Configured firebase database

--- BuggedOut/app/core/service/firebase-config.service.ts
+++ BuggedOut/app/core/service/firebase-config.service.ts
@@ -1,15 +1,23 @@
 import {Injectable} from "@angular/core";
 import {FIREBASE_CONFIG} from "../constants";
 import * as firebase from "firebase";
-
+require('firebase/database');
 
 @Injectable()
 export class FirebaseConfigService {
+
+    private database: firebase.database.Database;
+
     constructor() {
         this.configureApp();
+        this.configDatabase();
     }
 
     configureApp() {
         firebase.initializeApp(FIREBASE_CONFIG);
+    }
+
+    configDatabase () {
+        this.database = firebase.database();
     }
 }
