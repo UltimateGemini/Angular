ac183b8826e282583a3e09f7a652042fb6c7f3d4
Noel F <nfranceschijr@gmail.com>
2019-10-23 22:00:47 -0400

    passed production build

--- fitness-tracker/src/environments/environment.prod.ts
+++ fitness-tracker/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBDaL7ZhfB8pDw0Ey_a-_kyEelch0qAl4g',
+    authDomain: 'ng-fitness-tracker-ec022.firebaseapp.com',
+    databaseURL: 'https://ng-fitness-tracker-ec022.firebaseio.com',
+    projectId: 'ng-fitness-tracker-ec022',
+    storageBucket: 'ng-fitness-tracker-ec022.appspot.com',
+    messagingSenderId: '399911960462',
+    appId: '1:399911960462:web:606fdfda132ba3401baedf'
+  }
 };
